a8ba4f74f04f604794151dc219152ae620ef9ef5
Jop <jop.verhoeven@gmail.com>
2019-09-17 08:40:11 +0200

    Start of profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,3 +1,6 @@
+import { User } from './../../models/User';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthenticationService } from './../../services/application/authentication/authentication.service';
 import { ProfileService } from './../../services/application/profile/profile.service';
 import { Profile } from './../../models/Profile';
 import { Component, OnInit } from '@angular/core';
@@ -12,10 +15,13 @@ export class ProfileComponent implements OnInit {
 
   profile: Profile;
   doneLoading = false;
+  user: User;
 
   constructor(
     private profileService: ProfileService,
     private route: ActivatedRoute,
+    private authService: AuthenticationService,
+    private cookieService: CookieService,
   ) { }
 
   async ngOnInit() {
@@ -23,12 +29,21 @@ export class ProfileComponent implements OnInit {
     const username = this.route.snapshot.paramMap.get('id');
 
     await this.getProfile(username);
+    await this.getUser();
 
     this.doneLoading = true;
   }
 
   async getProfile(username: string) {
     await this.profileService.getProfile(username).then((data) => this.profile = data);
+  }
+
+  async getUser() {
+    if (this.cookieService.check('token')) {
+      const token = this.cookieService.get('token');
+      await this.authService.loginWithToken(token)
+      .then((data) => this.user = data);
+    }
   }
 
 }
