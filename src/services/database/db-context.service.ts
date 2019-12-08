08b9d2901ae218fa7d83b64bf320a0c3a340ff8e
Jop <jop.verhoeven@gmail.com>
2019-12-08 17:11:37 +0100

    Updated database URL

--- src/services/database/db-context.service.ts
+++ src/services/database/db-context.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class DbContextService {
 
-  public RestURL: String = 'https://jop.salvemundi.nl/';
+  public RestURL: String = 'http://84.83.195.78:8090/';
   // public AuthenticationURL: String = 'http://localhost:8091/';
 
   constructor() { }
